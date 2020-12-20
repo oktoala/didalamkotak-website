cebf3cd7c4cbd080a648a02dae88c07508df4385
Ian-syah <okta.yoga.syah@gmail.com>
2020-12-21 00:22:53 +0800

    :notes: Fix The Loader

--- themes/kotak/static/js/loading.js
+++ themes/kotak/static/js/loading.js
@@ -1,10 +1,4 @@
-// var myVar;
-//
-// function myFunction() {
-//   myVar = setTimeout(showPage, 300);
-// }
-//
-// function showPage() {
-//   document.getElementById("loader").style.display = "none";
-//   document.querySelector("container").style.display = "block";
-// }
+window.addEventListener("load", function(){
+  let loader = document.querySelector(".loader");
+  loader.className += " hidden";
+})
