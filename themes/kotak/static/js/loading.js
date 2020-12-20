2b4ce1f2a2f972b6c957ca1038ae2d12bd152424
Ian-syah <okta.yoga.syah@gmail.com>
2020-12-20 23:44:30 +0800

    :notes: Test The Loader

---
+++ themes/kotak/static/js/loading.js
@@ -0,0 +1,10 @@
+// var myVar;
+//
+// function myFunction() {
+//   myVar = setTimeout(showPage, 300);
+// }
+//
+// function showPage() {
+//   document.getElementById("loader").style.display = "none";
+//   document.querySelector("container").style.display = "block";
+// }
