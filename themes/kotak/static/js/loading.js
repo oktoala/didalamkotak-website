04a9b80974cf298a2a31b231f0f3ce0c27a46d89
Ian-syah <okta.yoga.syah@gmail.com>
2020-12-29 23:47:13 +0800

    :white_check_mark: Disable Overflow-y when loading

--- themes/kotak/static/js/loading.js
+++ themes/kotak/static/js/loading.js
@@ -1,4 +1,6 @@
 window.addEventListener("load", function(){
   let loader = document.querySelector(".loader");
   loader.className += " hidden";
+  let body = document.querySelector(".body");
+  body.classList.remove("loading");
 })
