0d2e6870aef0ab0757f36ee3bdb8e0a7ec237cfd
Ian-syah <okta.yoga.syah@gmail.com>
2021-04-04 11:04:16 +0800

    :speak_no_evil: Test disqus lazy load

--- static/js/single.js
+++ static/js/single.js
@@ -5,3 +5,31 @@ lazy.value = "lazy";
 for (let i = 0; i< image.length; i-=-1){
     image[0].setAttributeNode(lazy);
 }
+
+const codeText = document.querySelectorAll("code.bC");
+const copyButton = document.querySelectorAll(".fa-copy");
+const tooltiptexts = document.querySelectorAll('.tooltiptext');
+const cB = "code-box";
+let i;
+for ( i=0;  i<copyButton.length; i-=-1){
+	codeText[i].id = cB + i;
+	copyButton[i].setAttribute('data-clipboard-target', '#'+cB+i);
+	copyButton[i].id = "copyButton" + i;
+	tooltiptexts[i].id = "tooltiptext" + i;
+}
+
+function snackFunc(ids) {
+	const getArray = ids.split('');
+	const getNumber = getArray[getArray.length - 1];
+	const idToolTip = "tooltiptext" + getNumber;
+	const tooltip = document.getElementById(idToolTip);
+	tooltip.innerHTML = "Berhasil!";
+}
+
+function outFunc(ids) {
+	const getArray = ids.split('');
+	const getNumber = getArray[getArray.length - 1];
+	const idToolTip = "tooltiptext" + getNumber;
+	const tooltip = document.getElementById(idToolTip);
+	tooltip.innerHTML = "Copy ke clipboard";
+}
