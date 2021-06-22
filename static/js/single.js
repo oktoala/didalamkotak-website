1a9360b3d34e32fe6a029a913f2da71006b69ec4
oktoala <okta.yoga.syah@gmail.com>
2021-06-22 20:17:50 +0800

    Add Fullscreen

--- static/js/single.js
+++ static/js/single.js
@@ -2,13 +2,15 @@ const codeText = document.querySelectorAll("code.bC");
 const copyButton = document.querySelectorAll(".fa-copy");
 const tooltiptexts = document.querySelectorAll('.tooltiptext');
 const overlay = document.querySelectorAll(".overlay");
+const elementFull = document.documentElement;
+const iconFullScreen = document.querySelector(".overlay-header > .fa-expand");
 const cB = "code-box";
 let i;
 
 /* Looping for giving the id */
-for ( i=0;  i<copyButton.length; i-=-1){
+for (i = 0; i < copyButton.length; i -= -1) {
 	codeText[i].id = cB + i;
-	copyButton[i].setAttribute('data-clipboard-target', '#'+cB+i);
+	copyButton[i].setAttribute('data-clipboard-target', '#' + cB + i);
 	copyButton[i].id = "copyButton" + i;
 	tooltiptexts[i].id = "tooltiptext" + i;
 }
@@ -30,20 +32,36 @@ function outFunc(ids) {
 	tooltip.innerHTML = "Copy ke clipboard";
 }
 
-function imgClick(id){
+function imgClick(id) {
 	const imgClicked = document.getElementById(id);
 	imgClicked.classList.toggle("show");
+	closeFullScreen();
 }
 
-function goToSlide(go, id){
+function goToSlide(go, id) {
 	let index = 0;
 	const curr_id = document.getElementById(id);
-	for(let i = 0; i< overlay.length; i++){
-		if (overlay[i] == curr_id){
+	for (let i = 0; i < overlay.length; i++) {
+		if (overlay[i] == curr_id) {
 			index = i;
 		}
-	}	
+	}
+
 	curr_id.classList.toggle("show");
 	index += go;
 	overlay[index].classList.toggle("show");
+}
+
+function fullScreen() {
+	if (elementFull.requestFullscreen) {
+		iconFullScreen.classList.add("full");
+		elementFull.requestFullscreen();
+	}
+}
+
+function closeFullScreen() {
+	if (document.exitFullscreen) {
+		iconFullScreen.classList.remove("full");
+		document.exitFullscreen();
+	}
 }
