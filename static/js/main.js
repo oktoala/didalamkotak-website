a7a09fbe0c2d9eaef553a0ed00886fe322d1dc2f
oktoala <okta.yoga.syah@gmail.com>
2022-02-03 13:46:43 +0800

    Change the animation

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,7 @@
 const curr_themes = localStorage.getItem("theme");
 const body = document.querySelector(".body");
 if (curr_themes == "light") {
-	body.classList.add('light-theme');
+	body.classList.add("light-theme");
 }
 
 /* 
@@ -21,14 +21,13 @@ const logo_images = document.querySelector(".logo__imagebox");
 const bodies = document.querySelector("body");
 
 function logo_theme() {
-	bodies.classList.toggle('light-theme');
+	bodies.classList.toggle("light-theme");
 
 	let theme = "dark";
 	if (bodies.classList.contains("light-theme")) {
-		theme = "light"
+		theme = "light";
 	}
 	localStorage.setItem("theme", theme);
-
 }
 
 /* Slider Sidebar */
@@ -36,37 +35,43 @@ const slider = document.querySelector(".switch > input");
 const slider_li = document.querySelector("#slider_sidebar");
 const sidebar = document.querySelector(".sidebar");
 const primary = document.querySelector(".primary");
+const toc_class = document.querySelector("#toc");
 const curr_sidebar = localStorage.getItem("sidebarStorage");
 const curr_slider = localStorage.getItem("sliderStorage");
 const curr_primary = localStorage.getItem("primaryStorage");
-
 
 /* 
 ? Statement for check the sidebar that affect to slider on header */
 if (sidebar != null) {
 	if (curr_sidebar == "hidden") {
-		sidebar.classList.add('hidden');
+		sidebar.classList.add("hidden");
 		slider.checked = false;
 		primary.classList.add("full");
 	} else {
 		slider.checked = true;
 	}
-
 } else {
 	slider_li.classList.add("hidden");
 	slider.checked = false;
 	slider.disabled = true;
 	primary.classList.add("full");
-	document.getElementById("slider_sidebar").attributes["custom-title"].value = "Tidak Bisa";
+	document.getElementById("slider_sidebar").attributes["custom-title"].value =
+		"Tidak Bisa";
 }
 
 slider.addEventListener("click", () => {
+	toc_class.classList.toggle("absolute");
 	primary.classList.toggle("full");
 	let sidebar_class = "show";
 	let slider_class = "checked";
 	let primary_class = "unfull";
 
+	toc_class.addEventListener('transtitionrun', () => {
+		console.log('hahah');
+	});
+	
 	sidebar.classList.toggle("hidden");
+
 	if (sidebar.classList.contains("hidden")) {
 		slider.checked = false;
 		sidebar_class = "hidden";
@@ -79,18 +84,15 @@ slider.addEventListener("click", () => {
 	localStorage.setItem("sliderStorage", slider_class);
 	localStorage.setItem("primaryStorage", primary_class);
 	// sidebar.classList.remove("visible");
-
 });
 
 function buttonScroll(directions) {
-	if (directions == 'top') {
+	if (directions == "top") {
 		document.querySelector(".container").scrollIntoView(true);
 	} else {
 		document.querySelector(".container").scrollIntoView(false);
 	}
 }
-
-
 
 /* 
 ?Asyn Categories */
@@ -98,22 +100,19 @@ let current_taxo;
 function loadPage(newUrl) {
 	const httpRequest = new XMLHttpRequest();
 	httpRequest.onreadystatechange = function () {
-		if (httpRequest.readyState !== XMLHttpRequest.DONE)
-			return;
+		if (httpRequest.readyState !== XMLHttpRequest.DONE) return;
 
 		const newDocument = httpRequest.responseXML;
-		if (newDocument === null)
-			return;
+		if (newDocument === null) return;
 
 		const newContent = httpRequest.responseXML.querySelector("#main-page");
-		if (newContent === null)
-			return;
+		if (newContent === null) return;
 
 		document.title = newDocument.title;
 
 		const contentElement = document.querySelector("#main-page");
 		contentElement.replaceWith(newContent);
-	}
+	};
 
 	httpRequest.responseType = "document";
 	httpRequest.open("GET", newUrl);
@@ -123,7 +122,4 @@ function loadPage(newUrl) {
 	}
 	document.getElementById(newUrl).classList.toggle("current");
 	current_taxo = document.getElementById(newUrl);
-
-
-};
-
+}
