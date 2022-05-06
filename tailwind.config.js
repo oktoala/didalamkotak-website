1c6c72bd3f6178f917e8c0e2fd9d7a2fc984a22f
oktoala <okta.yoga.syah@gmail.com>
2022-05-06 21:08:50 +0800

    Change colors to tw colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: ['./layouts/**/*.html', './content/**/*.md'],
   theme: {
@@ -9,12 +10,12 @@ module.exports = {
         'dk-maxW': '950px'
       },
       colors: {
-        'dk-blue': '#8bd3e1',
-        'dk-primary': '#1e1e1e',
-        'dk-secondary': '#262627',
-        'dk-text': '#ffffff',
-        'dk-text-alpha': '#8a8a8a',
-        'dk-hl': '#fff400',
+        'dk-blue': colors.sky,
+        'dk-primary': colors.zinc,
+        'dk-secondary': colors.slate,
+        'dk-text': colors.white,
+        'dk-text-alpha': colors.slate,
+        'dk-hl': colors.yellow,
       },
       fontFamily: {
         'dk-sans': "'Merriweather', Roboto, Helvetica, Arial, sans-serif",
@@ -25,6 +26,9 @@ module.exports = {
       gridTemplateColumns: {
         'dk-grid': "repeat(auto-fit, minmax(300px, 395px))"
       },
+      borderRadius: {
+        '4xl': '2em'
+      }
     },
   },
   plugins: [],
