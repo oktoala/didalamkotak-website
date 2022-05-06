7cab0d82acb74dd528453d1924ea81607d24ddb3
oktoala <okta.yoga.syah@gmail.com>
2022-05-06 17:14:51 +0800

    Done the heading

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
 module.exports = {
   content: ['./layouts/**/*.html', './content/**/*.md'],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        'side': '20px',
+      },
+      maxWidth: {
+        'dk-maxW': '950px'
+      },
+      colors: {
+        'dk-blue': '#8bd3e1',
+        'dk-primary': '#1e1e1e',
+        'dk-secondary': '#262627',
+        'dk-text': '#ffffff',
+        'dk-hl': '#fff400',
+      },
+      fontFamily: {
+        'dk-sans': "'Merriweather', Roboto, Helvetica, Arial, sans-serif",
+        'dk-mono': "'Fira Code', sans-serif, monospace",
+        'dk-heading': "'Roboto', sans-serif",
+        'dk-fa': 'FontAwesome',
+      }
+    },
   },
   plugins: [],
 }
