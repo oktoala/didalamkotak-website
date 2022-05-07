1b0b6d7dc8740bbfc21d1d8c8eeb7d946a6e8c83
oktoala <okta.yoga.syah@gmail.com>
2022-05-07 16:56:23 +0800

    Done the search

--- static/js/search.js
+++ static/js/search.js
@@ -1,90 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     let searchResults = [];
-    const searchWrapper = document.querySelector("div[role=search]");
-    const searchResultElement = searchWrapper.querySelector(".search-results");
-    const searchInput = searchWrapper.querySelector("input");
-    const search_button = document.querySelector(".toggle-search");
-    // const burger = document.querySelector('.burger input');
-	const nav = document.querySelector('.navigation');
-    const nav_items = document.querySelectorAll(".nav-item");
-    const search_text = document.querySelector(".nav-item.show-search");
-
-    const index_nav_items = (function test(){
-        for (let i = 0; i < nav_items.length; i-=-1){
-            if (nav_items[i].classList.contains("current")){
-                return i;
-            }
-        }
-    })();
-
-    const toggleAllSearchItem = () => {
-        toggleSearch(searchWrapper, searchInput);
-        search_button.classList.toggle("active");
-        search_text.classList.toggle("current");
-        if (index_nav_items !== undefined){
-            nav_items[index_nav_items].classList.toggle("current");
-        }
-    }
-
-    /* Make always unchecked if reload */
-    // burger.checked = false;
-
-    /* Toggle For Search Button */
-    const toggleSearch = (searchWrapper, searchInput) => {
-        if (searchWrapper.classList.contains("active")) {
-            searchWrapper.classList.add("visible");
-            setTimeout(() => {
-                searchWrapper.classList.remove("visible");
-            }, 300);
-            searchWrapper.classList.remove("active");
-            
-        } else {
-            searchWrapper.classList.add("active");
-            searchInput.focus();
-        }
-    }
-
-    /* Toggle For Search Button and change the color of focus icon*/
-    search_button.addEventListener('click', () => {
-        toggleAllSearchItem();
-        if (searchWrapper.classList.contains("active")){
-            searchResultElement.innerHTML = "";
-            searchInput.value = null;
-        }
-    });
-
-    /* Make input for search can't press enter */
-    document.querySelector("form.search").addEventListener("keypress", (event)=>{
-        if (event.key == "Enter"){
-            event.preventDefault();
-        }
-    });
-
-
-    /* Close Search Field when button pressed */
-	// burger.addEventListener('click', function(){
-	// 	nav.classList.toggle('show');
-    //     if (searchWrapper.classList.contains("active")){
-    //         toggleAllSearchItem();
-    //     }
-	// });
-
-    /* Shortcut for Escape and open the search field */
-    window.addEventListener("keydown", e => {
-        // dismiss search on  ESC
-        if (e.key == "Escape" && searchWrapper.classList.contains("active")) {
-            e.preventDefault();
-            toggleAllSearchItem();
-        }
-
-        // open search on CTRL+SHIFT+F
-        if (e.ctrlKey && e.shiftKey && e.key == "F" && !searchWrapper.classList.contains("active")) {
-            e.preventDefault();
-            toggleAllSearchItem();
-            document.querySelector(".navigation").classList.add("show");
-            // burger.checked = true;
-        }
-    });
+    const searchInput = document.querySelector('#search-input');
+    const searchResultElement = document.querySelector('#search-results');
 
     const mark = (content, search) => {
         if (search) {
@@ -110,7 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 }
                 let re = new RegExp(s, "i");
                 content = content.replace(re, m => {
-                    return "<mark>" + m + "</mark>";
+                    return "<mark class='mark'>" + m + "</mark>";
                 });
             });
         }
@@ -153,9 +70,9 @@ document.addEventListener("DOMContentLoaded", () => {
                         let item = searchContent.find(el => {
                             return el.id == parseInt(match.ref);
                         });
-                        return "<li>" +
-                            "<h4><a href='" + item.url + "'>" + mark(item.title, searchString) + "</a></h4>" +
-                            "<p class='summary'>" +
+                        return "<li class='results mb-5'>" +
+                            "<h4 ><a class='result-h' href='" + item.url + "'>" + mark(item.title, searchString) + "</a></h4>" +
+                            "<p class='summary text-dk-text-alpha-500 text-xs leading-6 mt-4'>" +
                             mark((item.content.length > 200 ? (item.content.substring(0, 200) + "...") : item.content), searchString) +
                             "</p>" +
                             "</li>";
